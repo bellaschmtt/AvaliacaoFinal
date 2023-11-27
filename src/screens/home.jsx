158ebfcef4ae60b5eb398ffa409328099e27bdf3
isabella <isabellaschmitz00@gmail.com>
2023-11-27 14:18:56 -0300

    oi2

---
+++ src/screens/home.jsx
@@ -0,0 +1,54 @@
+import {View, StyleSheet, Text, Button} from 'react-native';
+// import Header from '../components/Header';
+
+
+export const styles = StyleSheet.create({
+    container: {
+        // backgroundColor: "purple",
+    //   flex: 1 ,
+      justifyContent: "center",
+    //   alignItems: "center",
+      padding: 10,
+      top: 10,
+     
+    
+    },
+    content: {
+        fontSize: 20,
+      
+    } 
+   
+});
+
+
+export default function Home ({navigation}) {
+
+    return (
+        <View style={styles.container}>
+
+            {/* <View style={{ backgroundColor: "green" }}> 
+                <Header title="Orientação"/>
+            </View> */}
+
+            <Text style={styles.content}>
+                Bem-vindo
+            </Text>
+            <View style={{ marginTop: 10 }} />
+            <Button 
+                mode="contained"
+                onPress={() => navigation.navigate('DeviceInfo')}
+                title="Ir para DeviceInfo"
+            />
+            <View style={{ marginTop: 10 }} />
+            <Button 
+                // mode="contained"
+                onPress={() => navigation.navigate('BatteryInfo')}
+                title="Ir para Bateria e afins"
+                
+            />
+            <View style={{ marginTop: 10 }} />
+           
+           
+        </View>
+    )
+}
